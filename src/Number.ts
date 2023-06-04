4aba97e3b13e178be54d337718c725a42fa3199f
Aomd <g00665@163.com>
2023-06-04 07:29:11 +0000

    添加 u8 与字符串互转  浮点型与 u8 互转

--- src/Number.ts
+++ src/Number.ts
@@ -23,3 +23,39 @@ export function random(min: number, max: number): number {
 export function clamp(value: number, min: number, max: number) {
   return Math.min(Math.max(value, min), max)
 }
+
+/**
+ * float32 To uint8Array
+ * @param value
+ * @returns
+ */
+export function f32ToU8(value: number): Uint8Array {
+  return new Uint8Array(new Float32Array([value]).buffer);
+}
+
+/**
+ * uint8Array To float32
+ * @param u8
+ * @returns
+ */
+export function u8ToF32(u8: Uint8Array): number {
+  return new Float32Array(u8.buffer)[0];
+}
+
+/**
+ * float64 To uint8Array
+ * @param value
+ * @returns
+ */
+export function f64ToU8(value: number): Uint8Array {
+  return new Uint8Array(new Float64Array([value]).buffer);
+}
+
+/**
+ * uint8Array To float64
+ * @param u8
+ * @returns
+ */
+export function u8ToF64(u8: Uint8Array): number {
+  return new Float64Array(u8.buffer)[0];
+}
--- src/String.ts
+++ src/String.ts
@@ -12,11 +12,35 @@ export function stringToCharCodes(value: string): number[] {
 }
 
 /**
+ * 兼容函数
  * 字符串转 Uint8Array
+ * Text编解码不能使用时采用 unicode 两字节实现中文编码
  * @param str
  * @returns
  */
 export function stringToUint8Array(str: string): Uint8Array {
-  const encoder = new TextEncoder();
-  return encoder.encode(str);
+  if (TextEncoder && TextDecoder) {
+    const encoder = new TextEncoder();
+    return encoder.encode(str);
+  } else {
+    // 使用两字节兼容大部常用中文
+    return new Uint8Array(new Uint16Array(stringToCharCodes(str)).buffer)
+  }
+
+}
+
+/**
+ * 兼容函数
+ * Uint8Array 转 字符串
+ * Text编解码不能使用时采用 unicode 两字节实现中文编码
+ * @param u8
+ * @returns
+ */
+export function uint8ArrayToString(u8: Uint8Array): string {
+  if (TextEncoder && TextDecoder) {
+    const decoder = new TextDecoder()
+    return decoder.decode(u8)
+  } else {
+    return String.fromCharCode(...new Uint16Array(u8.buffer))
+  }
 }
