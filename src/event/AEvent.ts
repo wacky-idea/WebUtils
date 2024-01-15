40ebb110301e3a033d3e1195f27035c5784e019e
Aomd <g00665@163.com>
2024-01-15 15:17:47 +0800

    添加 AEvent 注释

--- src/event/AEvent.ts
+++ src/event/AEvent.ts
@@ -1,10 +1,24 @@
 export class AEvent<T> {
+
+  /**
+   * 事件列表
+   *
+   * @private
+   * @memberof AEvent
+   */
   private eventListeners: Map<string, ((data: T) => void)[]>;
 
   constructor() {
     this.eventListeners = new Map();
   }
 
+  /**
+   * 监听事件
+   *
+   * @param {string} eventName
+   * @param {(data: T) => void} handler
+   * @memberof AEvent
+   */
   on(eventName: string, handler: (data: T) => void): void {
     if (!this.eventListeners.has(eventName)) {
       this.eventListeners.set(eventName, []);
@@ -12,14 +26,28 @@ export class AEvent<T> {
     this.eventListeners.get(eventName)?.push(handler);
   }
 
-  one(eventName: string, handler: (data: T) => void): void {
-    const oneTimeHandler = (data: T) => {
+  /**
+   * 单次监听
+   *
+   * @param {string} eventName
+   * @param {(data: T) => void} handler
+   * @memberof AEvent
+   */
+  once(eventName: string, handler: (data: T) => void): void {
+    const onceTimeHandler = (data: T) => {
       handler(data);
-      this.off(eventName, oneTimeHandler);
+      this.off(eventName, onceTimeHandler);
     };
-    this.on(eventName, oneTimeHandler);
+    this.on(eventName, onceTimeHandler);
   }
 
+  /**
+   * 取消监听
+   *
+   * @param {string} eventName
+   * @param {(data: T) => void} handler
+   * @memberof AEvent
+   */
   off(eventName: string, handler: (data: T) => void): void {
     const listeners = this.eventListeners.get(eventName);
     if (listeners) {
@@ -30,6 +58,13 @@ export class AEvent<T> {
     }
   }
 
+  /**
+   * 触发事件
+   *
+   * @param {string} eventName
+   * @param {T} data
+   * @memberof AEvent
+   */
   emit(eventName: string, data: T): void {
     const listeners = this.eventListeners.get(eventName);
     if (listeners) {
